386e27be9ab8c477704fde82d5cb930be9d9550c
Maxime <maxime@iMac-de-Maxime.local>
2024-08-18 11:46:05 +0200

    Send ACK after receiving a FIN request

--- src/packets.rs
+++ src/packets.rs
@@ -8,6 +8,7 @@ pub mod packets {
         Listen,
         SynReceived,
         Established,
+        LastAck,
     }
 
     pub struct TcpTlb {
@@ -35,6 +36,7 @@ pub mod packets {
                 TcpState::Listen => self.on_syn_request(&tcphdr, response)?,
                 TcpState::SynReceived => self.on_ack_request(&tcphdr)?,
                 TcpState::Established => self.on_data_request(&tcphdr, payload, response)?,
+                TcpState::LastAck => (),
             }
 
             Ok(())
@@ -104,6 +106,10 @@ pub mod packets {
             .expect("Builder failed");
 
             self.payload.extend(payload.iter());
+
+            if tcphdr.fin {
+                self.state = TcpState::LastAck;
+            }
 
             Ok(())
         }
@@ -230,6 +236,76 @@ mod tests {
             10,    // windows size)
         )
         .ack(resp_tcphdr.sequence_number + 1)
+        .write(&mut request, payload)
+        .unwrap();
+
+        request
+    }
+
+    #[test]
+    fn send_fin_packet_close_server_connection() {
+        const CLIENT_SEQNUM: u32 = 100;
+
+        let ip = Ipv4Addr::from([192, 168, 1, 2]);
+        let mut connections: HashMap<Connection, TcpTlb> = HashMap::new();
+        let mut response_data: Vec<u8> = Vec::new();
+        let mut response_fin: Vec<u8> = Vec::new();
+
+        let response_syn = do_handshake(&mut connections, &ip, CLIENT_SEQNUM);
+
+        let data = [1, 2, 3];
+        let data_packet = build_ack_request(&data, CLIENT_SEQNUM + 1, &response_syn);
+        on_request(&data_packet, &mut response_data, &mut connections, &ip).unwrap();
+
+        let seqnum = CLIENT_SEQNUM + 1 + (data.len() as u32);
+        let fin_packet = build_fin_request(&[], seqnum, &response_data);
+        on_request(&fin_packet, &mut response_fin, &mut connections, &ip).unwrap();
+
+        // Check responses
+        let (_, resp_tcphdr) = Ipv4Header::from_slice(&response_fin[..]).unwrap();
+        let (resp_tcphdr, _) = TcpHeader::from_slice(resp_tcphdr).unwrap();
+
+        assert_eq!(resp_tcphdr.acknowledgment_number, 104);
+        assert_eq!(resp_tcphdr.ack, true);
+    }
+
+    fn do_handshake(
+        connections: &mut HashMap<Connection, TcpTlb>,
+        ip: &Ipv4Addr,
+        seqnum: u32,
+    ) -> Vec<u8> {
+        let mut response_syn: Vec<u8> = Vec::new();
+        let mut response_ack: Vec<u8> = Vec::new();
+
+        let syn_packet = build_syn_request(seqnum);
+        on_request(&syn_packet, &mut response_syn, connections, ip).unwrap();
+
+        let ack_packet = build_ack_request(&[], seqnum + 1, &response_syn);
+        on_request(&ack_packet, &mut response_ack, connections, ip).unwrap();
+
+        // return response_sync because response_ack is empty after sending a ack without data
+        response_syn
+    }
+
+    fn build_fin_request(payload: &[u8], seq: u32, response_syn: &[u8]) -> Vec<u8> {
+        let mut request: Vec<u8> = Vec::new();
+
+        let (_, resp_tcphdr) = Ipv4Header::from_slice(&response_syn[..]).unwrap();
+        let (resp_tcphdr, _) = TcpHeader::from_slice(resp_tcphdr).unwrap();
+
+        PacketBuilder::ipv4(
+            [192, 168, 1, 1], // source
+            [192, 168, 1, 2], // destination
+            2,                // ttl
+        )
+        .tcp(
+            35000, // source
+            22,    //destination
+            seq,   //seq
+            10,    // windows size)
+        )
+        .ack(resp_tcphdr.sequence_number + 1)
+        .fin()
         .write(&mut request, payload)
         .unwrap();
 
