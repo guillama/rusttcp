7ba8cfc096e456f07c78b7190ba206d745a8096e
Maxime <maxime@iMac-de-Maxime.local>
2024-10-11 13:31:52 +0200

    Add a test to check that the client resets its timeout
    After receiving a response from the server, the client will reset its timer.

--- tests/test.rs
+++ tests/test.rs
@@ -709,9 +709,60 @@ fn client_retransmits_data_by_doubling_the_timeout_between_successive_retransmis
     assert_eq!(client_packet_not_retransmitted4.len(), 0);
     assert_eq!(client_packet_retransmitted5.len(), 45);
     assert_eq!(client_packet1, client_packet_retransmitted1);
-    assert_eq!(client_packet_retransmitted1, client_packet_retransmitted2);
-    assert_eq!(client_packet_retransmitted2, client_packet_retransmitted3);
-    assert_eq!(client_packet_retransmitted3, client_packet_retransmitted4);
-    assert_eq!(client_packet_retransmitted4, client_packet_retransmitted5);
+    assert_eq!(client_packet1, client_packet_retransmitted2);
+    assert_eq!(client_packet1, client_packet_retransmitted3);
+    assert_eq!(client_packet1, client_packet_retransmitted4);
+    assert_eq!(client_packet1, client_packet_retransmitted5);
     assert_eq!(client_packet_retransmitted6, Vec::new());
+}
+
+#[test]
+fn client_resets_its_timer_after_receiving_a_response() {
+    const WINDOW_SIZE: u16 = 5;
+
+    let fake_timer = Rc::new(RefCell::new(Timer::now()));
+    let mut client: RustTcp = RustTcp::new([192, 168, 1, 1])
+        .timer(fake_timer.clone())
+        .tcp_retries(5);
+    let mut server: RustTcp = RustTcp::new([192, 168, 1, 2]).window_size(WINDOW_SIZE);
+
+    // 3-way handshake
+    let _ = do_handshake(&mut client, &mut server);
+
+    // Send data bigger than the sending window size
+    let data = &[1, 2, 3, 4, 5, 6, 7, 8];
+    client.write("client", data).unwrap();
+    let client_packet1 = process_user_event(&mut client);
+
+    fake_timer.borrow_mut().add_millisecs(200);
+    let client_packet_retransmitted1 = process_timeout_event(&mut client);
+
+    fake_timer.borrow_mut().add_millisecs(400);
+    let client_packet_retransmitted2 = process_timeout_event(&mut client);
+
+    let mut server_ack = Vec::new();
+    server.on_packet(&client_packet1, &mut server_ack).unwrap();
+    client.on_packet(&server_ack, &mut Vec::new()).unwrap();
+
+    let client_packet2 = process_user_event(&mut client);
+
+    fake_timer.borrow_mut().add_millisecs(200);
+    let client_packet_retransmitted3 = process_timeout_event(&mut client);
+
+    fake_timer.borrow_mut().add_millisecs(400);
+    let client_packet_retransmitted4 = process_timeout_event(&mut client);
+
+    assert_eq!(client_packet1.len(), 45);
+    assert_eq!(client_packet_retransmitted1.len(), 45);
+    assert_eq!(client_packet_retransmitted2.len(), 45);
+
+    assert_eq!(client_packet2.len(), 43);
+    assert_eq!(client_packet_retransmitted3.len(), 43);
+    assert_eq!(client_packet_retransmitted4.len(), 43);
+
+    assert_eq!(client_packet1, client_packet_retransmitted1);
+    assert_eq!(client_packet1, client_packet_retransmitted2);
+
+    assert_eq!(client_packet2, client_packet_retransmitted3);
+    assert_eq!(client_packet2, client_packet_retransmitted4);
 }
