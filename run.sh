9cde4301bafe8ba0640e135f19ecc110b89047a2
Maxime <maxime@iMac-de-Maxime.local>
2024-11-07 15:15:10 +0100

    Clean up script run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
 cargo build || exit 1
-sudo route -n delete -net 10.0.0.0/24 10.0.0.1
-#sudo route -n add -net 10.0.0.0/24 10.0.0.1
-sudo target/debug/rusttcp 10.0.0.1
+target/debug/rusttcp 10.0.0.1
